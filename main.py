80475ec44b7b307f821d5d4dc894de2054532a41
xuhui <ofxuhui@gmail.com>
2024-10-08 19:13:38 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import asyncio
+import os.path
+import sys
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(sys.version)
+    path=os.path.abspath(".")
+    print(path)
+    print(os.path.curdir)
+    print(os.path.abspath(os.path.curdir))
+    print(os.EX_OK)
+    print(os.O_CREAT)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
